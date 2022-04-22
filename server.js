e25bcab9b7f63bc66c406e17de9092396c62bc78
Matt <matthieu.dev.degny@gmail.com>
2022-04-22 06:21:35 +0200

    file .env add

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const nodemailer = require("nodemailer");
 
 app.use(express.static('public'));
@@ -8,6 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req,res) =>{
     res.sendFile(__dirname + '/public/pf.html')
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.post('/', (req,res) => {
 
@@ -41,6 +44,6 @@ app.post('/', (req,res) => {
 
 })
 
-app.listen(5000, () => {
-    console.log(`Server listening on http://localhost:5000`);
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:3000`);
 })
