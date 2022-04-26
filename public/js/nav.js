e7317f1a3c6f853185f7d846955a58cce82690c4
Matt <matthieu.dev.degny@gmail.com>
2022-04-26 17:39:36 +0200

    nav.js file change in a clean object*/

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,76 +1,103 @@
-/*Nav move*/
-/*faire remonter-descendre la nabBar au clique du bouton Nav*/
-const button = document.querySelector(".menu-nav-button")
-const cross = document.getElementById("logo-cross")
-const navBar= document.querySelector(".menu-nav");
+const nav = {
 
-animationNavBar = () => {
-  navBar.classList.toggle("menu-nav-animation");
-  button.classList.toggle("menu-nav-button-animation");
-  cross.classList.toggle("logoCross-desappear")};
+    objectsNavbar :{
+      button : document.querySelector(".menu-nav-button"),
+      cross : document.getElementById("logo-cross"),
+      navBar: document.querySelector(".menu-nav")
+    },
 
-button.addEventListener("click", animationNavBar)
+    objectsLinksNavbar : {
+      homeLink : document.getElementById("link-home"),
+      aboutLink : document.getElementById("link-about"),
+      skillsLink : document.getElementById("link-skills"),
+      pfLink : document.getElementById("link-pf"),
+      contactLink : document.getElementById("link-contact")
+    },
 
+    objectsSections:{
+      sectionOne : document.getElementById("home"),
+      sectionTwo : document.getElementById("about"),
+      sectionThree : document.getElementById("skills"),
+      sectionFour : document.getElementById("pf"),
+      sectionFive : document.getElementById("contact")
+    },
 
-/*animation navbar
-Les liens sont animés d'un sous-lignement lorsque la section correspondante est visitée par l'utilisateur*/
-const sectionOne = document.getElementById("home")
-const sectionTwo = document.getElementById("about")
-const sectionThree = document.getElementById("skills")
-const sectionFour = document.getElementById("pf")
-const sectionFive = document.getElementById("contact")
+    initListeners: function() {
+      nav.objectsNavbar.button.addEventListener("click", nav.actionListener.animationNavBar)
+    },
 
-let optionsLink = { 
-    threshold: 0.42,}
+    actionListener: {
+      /*the action is to make up the navabr, it will be hidden,when the button is pressed
+      and make down, when the user press it back (thanks toggle)*/
+      animationNavBar: function () {
+        nav.objectsNavbar.navBar.classList.toggle("menu-nav-animation")
+        nav.objectsNavbar.button.classList.toggle("menu-nav-button-animation")
+        nav.objectsNavbar.cross.classList.toggle("logoCross-desappear")
+      }
+    },
+
+    /*the goal is to make animated the navbar, depending on where is the user*/
+    initObservers:function () {
+      /*creation of a new instance of the Class IntersectionObserver(connected with a method handleIntersectionOne, and the setting optionsLink)*/
+      const observerSectionOne = new IntersectionObserver(nav.actionsObserver.handleIntersectsectionOne, nav.optionObserver.optionsLink);
+      /*the new obsever is set on sectionOne*/
+      observerSectionOne.observe(nav.objectsSections.sectionOne)
+
+      const observerSectionTwo = new IntersectionObserver(nav.actionsObserver.handleIntersectsectionTwo, nav.optionObserver.optionsLink)
+      observerSectionTwo.observe(nav.objectsSections.sectionTwo)
+
+      const observerSectionThree = new IntersectionObserver(nav.actionsObserver.handleIntersectsectionThree, nav.optionObserver.optionsLink)
+      observerSectionThree.observe(nav.objectsSections.sectionThree)
+
+      const observerSectionFour = new IntersectionObserver(nav.actionsObserver.handleIntersectsectionFour, nav.optionObserver.optionsLink)
+      observerSectionFour.observe(nav.objectsSections.sectionFour)
+
+      const observerSectionFive = new IntersectionObserver(nav.actionsObserver.handleIntersectsectionFive, nav.optionObserver.optionsLink)
+      observerSectionFive.observe(nav.objectsSections.sectionFive)
+    },
     
-  /*nav home*/
-  const homeLink = document.getElementById("link-home");
-  function handleIntersectsectionOne(entries){
-    entries.forEach(entry => {
-      if(entry.isIntersecting){homeLink.classList.add("link")}
-      else{homeLink.classList.remove("link")}
-    })}
-  const observerSectionOne = new IntersectionObserver(handleIntersectsectionOne, optionsLink)
-  observerSectionOne.observe(sectionOne)
+    actionsObserver:  {
+      /*the methid handleIntersectionOne:
+      if sectionOne is macthed, then a underline is add to the link homeLink
+      when the observer leave sectionOne, the underline is remove*/
+      handleIntersectsectionOne: function (entries){
+        entries.forEach(entry => {
+          if(entry.isIntersecting){nav.objectsLinksNavbar.homeLink.classList.add("link")}
+          else{nav.objectsLinksNavbar.homeLink.classList.remove("link")}
+        })},
+
+      handleIntersectsectionTwo: function(entries){
+        entries.forEach(entry => {
+          if(entry.isIntersecting){nav.objectsLinksNavbar.aboutLink.classList.add("link")}
+          else{nav.objectsLinksNavbar.aboutLink.classList.remove("link")}
+        })},
+
+      handleIntersectsectionThree: function(entries){
+        entries.forEach(entry => {
+          if(entry.isIntersecting){nav.objectsLinksNavbar.skillsLink.classList.add("link")}
+          else{nav.objectsLinksNavbar.skillsLink.classList.remove("link")}
+        })},
+
+       handleIntersectsectionFour: function(entries){
+        entries.forEach(entry => {
+          if(entry.isIntersecting){nav.objectsLinksNavbar.pfLink.classList.add("link")}
+          else{nav.objectsLinksNavbar.pfLink.classList.remove("link")}
+        })},
+
+        handleIntersectsectionFive: function(entries){
+        entries.forEach(entry => {
+          if(entry.isIntersecting){nav.objectsLinksNavbar.contactLink.classList.add("link")}
+          else{nav.objectsLinksNavbar.contactLink.classList.remove("link")}
+        })}
+    },
     
-  /*nav about*/
-  const aboutLink = document.getElementById("link-about");
-  function handleIntersectsectionTwo(entries){
-    entries.forEach(entry => {
-      if(entry.isIntersecting){aboutLink.classList.add("link")}
-      else{aboutLink.classList.remove("link")}
-    })}
-  const observerSectionTwo = new IntersectionObserver(handleIntersectsectionTwo, optionsLink)
-  observerSectionTwo.observe(sectionTwo)
-  
-  /*nav skills*/
-  const skillsLink = document.getElementById("link-skills");
-  function handleIntersectsectionThree(entries){
-    entries.forEach(entry => {
-      if(entry.isIntersecting){skillsLink.classList.add("link")}
-      else{skillsLink.classList.remove("link")}
-    })}
-  const observerSectionThree = new IntersectionObserver(handleIntersectsectionThree, optionsLink)
-  observerSectionThree.observe(sectionThree)
-  
-  /*nav pf*/
-  const pfLink = document.getElementById("link-pf");
-  function handleIntersectsectionFour(entries){
-    entries.forEach(entry => {
-      if(entry.isIntersecting){
-       pfLink.classList.add("link")}
-      else{pfLink.classList.remove("link")}
-    })}
-  const observerSectionFour = new IntersectionObserver(handleIntersectsectionFour, optionsLink)
-  observerSectionFour.observe(sectionFour)
-  
-  /*nav contact*/
-  const contactLink = document.getElementById("link-contact");
-  function handleIntersectsectionFive(entries){
-    entries.forEach(entry => {
-      if(entry.isIntersecting){contactLink.classList.add("link")}
-      else{contactLink.classList.remove("link")}
-    })}
-  const observerSectionFive = new IntersectionObserver(handleIntersectsectionFive, optionsLink)
-  observerSectionFive.observe(sectionFive)
+    /*how observers are setted to intersect targets*/
+    optionObserver : {
+      optionsLink : {threshold: 0.42,}
+    },
+}
+// when the loading is finish i launch nav.init
+document.addEventListener('DOMContentLoaded', nav.initListeners );
+document.addEventListener('DOMContentLoaded', nav.initObservers );
+
   
