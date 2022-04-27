0aedecb5380596d592cb04abe751a4f2fb9391ed
Matt <matthieu.dev.degny@gmail.com>
2022-04-27 10:35:59 +0200

    file contact.js change in a clean object

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,24 +1,40 @@
+const contact = {
+    initContact:function(){
+        contact.listener();
+    },
+
+    dataForm:{
+        contactForm : document.querySelector('.contact-form'),
+        email : document.querySelector(".contact-email"),
+        message : document.querySelector(".contact-text")
+    },
+    /*press button trigger the submission*/
+    listener: function(){
+        contact.dataForm.contactForm.addEventListener('submit', request)
+    },
+    
+    /*the submission request the url (/) with ther server.js file
+    no data used hier i send back nothing to the front*/
+    actionListener: {
+        request : (e) => {
+            fetch('/')
+            .then( res => {
+                if(res.ok){
+                    window.alert("Votre email a bien été envoyé")
+                    contact.dataForm.email.value = '';
+                    contact.dataForm.message.value = '';
+                }else{
+                    window.alert("Une erreur est survenue, merci d'utiliser le lien Linkedin")
+                    console.log("ERREUR");
+                }
+            })
+        }
+    }
+}
+// when the loading is finish i launch contact.init
+document.addEventListener('DOMContentLoaded', contact.initContact);
 
 
-const contactForm = document.querySelector('.contact-form');
-let email = document.querySelector(".contact-email");
-let message = document.querySelector(".contact-text");
+  
 
-contactForm.addEventListener('submit', (e) => {
-    let formData = {
-        email: email.value,
-        message:message.value,}
-
-        fetch('/')
-        .then( res => {
-            if(res.ok){
-                window.alert("Votre email a bien été envoyé")
-                email.value = '';
-                message.value = '';
-            }else{
-                window.alert("Une erreur est survenue, merci d'utiliser le lien Linkedin")
-                console.log("ERREUR");
-            }
-        })
-})    
         
