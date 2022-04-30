8cfa10f2bf2b9142e33b6d9bf2cc1ceb93f7a4a8
Matt <matthieu.dev.degny@gmail.com>
2022-04-30 14:33:18 +0200

    reuniting all observers in one file

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,7 +2,7 @@ const nav = {
 
     init:function(){
       nav.Listeners();
-      nav.Observers();
+      nav.observers();
     },
 
     objectsNavbar :{
@@ -31,72 +31,44 @@ const nav = {
       nav.objectsNavbar.button.addEventListener("click", nav.actionListener.animationNavBar)
     },
 
-    actionListener: {
+    actionListener:{
       /*the action is to make up the navabr, it will be hidden,when the button is pressed
       and make down, when the user press it back (thanks toggle)*/
       animationNavBar: function () {
-        nav.objectsNavbar.navBar.classList.toggle("menu-nav-animation")
-        nav.objectsNavbar.button.classList.toggle("menu-nav-button-animation")
-        nav.objectsNavbar.cross.classList.toggle("logoCross-desappear")
+        const {navBar, button,cross} = nav.objectsNavbar
+        navBar.classList.toggle("menu-nav-animation")
+        button.classList.toggle("menu-nav-button-animation")
+        cross.classList.toggle("logoCross-desappear")
       }
-    },
-
-    /*the goal is to make animated the navbar, depending on where is the user*/
-    Observers:function () {
-      function createNewObserver(handleIntersect,target){
-        /*creation of a new instance of the Class IntersectionObserver*/
-        const newObserver = new IntersectionObserver(handleIntersect, nav.optionObserver.optionsLink)
-        /*target is setted for newObserver*/
-        newObserver.observe(target)
-      }
-
-      createNewObserver(nav.actionsObserver.handleIntersectsectionOne,nav.objectsSections.sectionOne)
-      createNewObserver(nav.actionsObserver.handleIntersectsectionTwo,nav.objectsSections.sectionTwo)
-      createNewObserver(nav.actionsObserver.handleIntersectsectionThree,nav.objectsSections.sectionThree)
-      createNewObserver(nav.actionsObserver.handleIntersectsectionFour,nav.objectsSections.sectionFour)
-      createNewObserver(nav.actionsObserver.handleIntersectsectionFive,nav.objectsSections.sectionFive)
-
-    },
-    
-    actionsObserver:  {
-      /*the methid handleIntersectionOne:
-      if sectionOne is macthed, then a underline is add to the link homeLink
-      when the observer leave sectionOne, the underline is remove*/
-      handleIntersectsectionOne: function (entries){
-        entries.forEach(entry => {
-          if(entry.isIntersecting){nav.objectsLinksNavbar.homeLink.classList.add("link")}
-          else{nav.objectsLinksNavbar.homeLink.classList.remove("link")}
-        })},
-
-      handleIntersectsectionTwo: function(entries){
-        entries.forEach(entry => {
-          if(entry.isIntersecting){nav.objectsLinksNavbar.aboutLink.classList.add("link")}
-          else{nav.objectsLinksNavbar.aboutLink.classList.remove("link")}
-        })},
-
-      handleIntersectsectionThree: function(entries){
-        entries.forEach(entry => {
-          if(entry.isIntersecting){nav.objectsLinksNavbar.skillsLink.classList.add("link")}
-          else{nav.objectsLinksNavbar.skillsLink.classList.remove("link")}
-        })},
-
-       handleIntersectsectionFour: function(entries){
-        entries.forEach(entry => {
-          if(entry.isIntersecting){nav.objectsLinksNavbar.pfLink.classList.add("link")}
-          else{nav.objectsLinksNavbar.pfLink.classList.remove("link")}
-        })},
-
-        handleIntersectsectionFive: function(entries){
-        entries.forEach(entry => {
-          if(entry.isIntersecting){nav.objectsLinksNavbar.contactLink.classList.add("link")}
-          else{nav.objectsLinksNavbar.contactLink.classList.remove("link")}
-        })}
-    },
+    },    
     
     /*how observers are setted to intersect targets*/
     optionObserver : {
       optionsLink : {threshold: 0.42,}
     },
+
+    createObserver: (target,trigger) => {
+      function handleIntersection(entries){
+          entries.forEach(entry=> {
+            if(entry.isIntersecting){target.classList.add("link")}
+            else{target.classList.remove("link")}
+          })
+        }
+        const observer = new IntersectionObserver(handleIntersection, nav.optionObserver.optionsLink)
+        observer.observe(trigger)
+    },
+
+    observers:() => {
+      const {sectionOne,sectionTwo,sectionThree,sectionFour,sectionFive} = nav.objectsSections
+      const{homeLink, aboutLink, skillsLink, pfLink, contactLink} = nav.objectsLinksNavbar
+      nav.createObserver(homeLink,sectionOne)
+      nav.createObserver(aboutLink,sectionTwo)
+      nav.createObserver(skillsLink,sectionThree)
+      nav.createObserver(pfLink,sectionFour)
+      nav.createObserver(contactLink,sectionFive)
+    }
+
+
 }
 // when the loading is finish i launch nav.init
 document.addEventListener('DOMContentLoaded', nav.init);
--- public/js/sections.js
+++ public/js/sections.js
@@ -4,11 +4,11 @@ const sections = {
     sections.observers();
   },
 
-  /*objects as targets of observers*/
-  targetsObservers: {
-    targetAboutText : document.querySelector(".about-text"),
-    targetSkills : document.getElementById("skills-front"),
-    /*every cardContainers in sections PF are target and object Animated*/
+  /*objects as triggers of observers*/
+  triggersObservers: {
+    triggerAboutText : document.querySelector(".about-text"),
+    triggerSkills : document.getElementById("skills-front"),
+    /*every cardContainers in sections PF are trigger and object Animated*/
     cardContainor : document.querySelectorAll(".card-container-after")
   },
 
@@ -22,79 +22,60 @@ const sections = {
 
   /*array used for the timing in section Skills, method handleIntersectSkills*/
   timingItemsSkills:{
-     front : [0.15, 0.35, 0.55, 0.75],
-     back : [ 0.8, 1, 1.2, 1.4 ,1.6]
+     front : [0.15, 0.35, 0.55, 0.75],back : [ 0.8, 1, 1.2, 1.4 ,1.6]
   },
 
   observers: function () {
-    /*section About*/
-    function createNewObserver(handleIntersect,target){
-      /*creation of a new instance of the Class IntersectionObserver*/
-      const newObserver = new IntersectionObserver(handleIntersect, sections.optionObserver.optionsSections)
-      /*target is setted for newObserver*/
-      newObserver.observe(target)
-    }
-    /*section About*/
-    createNewObserver(sections.actionsObservers.handleIntersectAbout,sections.targetsObservers.targetAboutText)
-    /*section Skills*/
-    createNewObserver(sections.actionsObservers.handleIntersectSkills,sections.targetsObservers.targetSkills)
-
-    /*section PF*/
-    const observerPfContainor = new IntersectionObserver(sections.actionsObservers.handleIntersectPfContainor, sections.optionObserver.optionsSections)
-    sections.targetsObservers.cardContainor.forEach(container => observerPfContainor.observe(container))
+    sections.createObserver(sections.triggersObservers.triggerAboutText,sections.actionIntersection.sectionAbout)
+    sections.createObserver(sections.triggersObservers.triggerSkills,sections.actionIntersection.sectionSkills) 
+    sections.createObserverPf()
   },
 
-  actionsObservers: {
-    /*section About*/
-    /*the method handleIntersectionAbout:
-      if targetAboutText is macthed, then one animation is added at three elements (with three differents delays)*/
-    handleIntersectAbout: function(entries,observer){
-      entries.forEach(entry => {
-        if(entry.isIntersecting){   
-          sections.objectsAnimatedWithObserver.aboutImage.style.animation = "fromtop 1.2s  forwards",
-          sections.targetsObservers.targetAboutText.style.animation = "fromtop 1.2s 0.3s forwards",
-          sections.objectsAnimatedWithObserver.aboutLinks.style.animation = "fromtop 1.2s 0.6s forwards"
-           /*the animation is done, doesnt need anymore to observe this object*/
-          observer.unobserve(entry.target);
-        } 
-      })
-    },
-    
-    /*section Skills*/
-    handleIntersectSkills: function(entries, observer){
-      entries.forEach(entry => {
-        if(entry.isIntersecting){
-          /*three objects animated by the observerIntersection fronTelements + backElements + additional-skills*/
-         for(let i = 0; i<sections.objectsAnimatedWithObserver.frontItems.length; i++){
-           /*two arrays ( front + back ) used as timing (one index -> one timing)*/
-           sections.objectsAnimatedWithObserver.frontItems[i].style.animation = `frontItems 0.7s ${sections.timingItemsSkills.front[i]}s forwards`
-         }
-         for(let i = 0; i<sections.objectsAnimatedWithObserver.backItems.length;i++){
-           sections.objectsAnimatedWithObserver.backItems[i].style.animation = `backItems 0.7s ${sections.timingItemsSkills.back[i]}s forwards`
-         }
-         document.querySelector("#additional-skills").style.animation = "additionalItems 1s 1.8s forwards"
-         observer.unobserve(entry.target);
-        }
-      })
-    },
+  createObserver: (trigger,action) => {
+    function handleIntersection(entries){
+        entries.forEach(entry=> {
+          if(entry.isIntersecting){ 
+          action()
+          }
+        })
+      }
+      const observer = new IntersectionObserver(handleIntersection, nav.optionObserver.optionsLink)    
+      observer.observe(trigger)
+  },
 
-    /*section PF*/
-    handleIntersectPfContainor: function(entries, observer){
-      entries.forEach(entry => {
-        if(entry.isIntersecting) {entry.target.classList.add("card-animation")
-        observer.unobserve(entry.target);}
+  createObserverPf:()=> {
+    function handleIntersect(entries){
+      entries.forEach(entry=> {
+        if(entry.isIntersecting) entry.target.classList.add("card-animation")
       })
     }
+    const observer = new IntersectionObserver(handleIntersect,nav.optionObserver.optionsLink)
+    sections.triggersObservers.cardContainor.forEach(container => {observer.observe(container)})
+  },
 
+  actionIntersection: {
+    sectionAbout : function() {
+      sections.objectsAnimatedWithObserver.aboutImage.style.animation = "fromtop 1.2s  forwards",
+      sections.triggersObservers.triggerAboutText.style.animation = "fromtop 1.2s 0.3s forwards",
+      sections.objectsAnimatedWithObserver.aboutLinks.style.animation = "fromtop 1.2s 0.6s forwards"
+    },
+    sectionSkills: function() {
+      /*three objects animated by the observerIntersection fronTelements + backElements + additional-skills*/
+      for(let i = 0; i<sections.objectsAnimatedWithObserver.frontItems.length; i++){
+        /*two arrays ( front + back ) used as timing (one index -> one timing)*/
+        sections.objectsAnimatedWithObserver.frontItems[i].style.animation = `frontItems 0.7s ${sections.timingItemsSkills.front[i]}s forwards`
+      }
+      for(let i = 0; i<sections.objectsAnimatedWithObserver.backItems.length;i++){
+        sections.objectsAnimatedWithObserver.backItems[i].style.animation = `backItems 0.7s ${sections.timingItemsSkills.back[i]}s forwards`
+      }
+      document.querySelector("#additional-skills").style.animation = "additionalItems 1s 1.8s forwards"
+    },
   },
 
   optionObserver : {
     optionsSections : {threshold: 0.75}
   },
 }
+
 // when the loading is finish i launch sections.init
-document.addEventListener('DOMContentLoaded', sections.init);
-
-
-  
-  
+document.addEventListener('DOMContentLoaded', sections.init)
