e179c2f767de0409dc31eaad56a2fd515d101304
Matt <matthieu.dev.degny@gmail.com>
2022-04-30 15:59:48 +0200

    after creation grid about, forgot to modify containor-picture -> about-image for the js observer = done

--- public/js/observer.js
+++ public/js/observer.js
@@ -27,7 +27,7 @@ const observer = {
         pfLink : document.getElementById("link-pf"),
         contactLink : document.getElementById("link-contact"),
         /*sections*/
-        aboutImage : document.querySelector(".container-picture"),
+        aboutImage : document.querySelector("#about-image"),
         aboutLinks : document.querySelector(".about-link"),
         frontItems : document.querySelectorAll(".item-front"),
         backItems : document.querySelectorAll(".item-back")
