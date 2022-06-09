2dbb9a99976fb38719ba06b244515101e652c0d7
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-06-09 19:27:16 +0200

    update docker

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:16
 WORKDIR /portfolio_matthieu
-COPY ./package.json .
+COPY package*.json ./
 RUN npm install
 COPY . .
 EXPOSE 3000
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 /*front side*/
 app.get('/', (req,res) =>{
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile('index.html')
 });
 
 /*back side (nodemailer)*/
