f67e04579aa2905714c6fdfef8101e2594d6e4ec
Matt <matthieu.dev.degny@gmail.com>
2022-06-11 12:11:21 +0200

    .env added

--- public/js/contact.js
+++ public/js/contact.js
@@ -8,47 +8,53 @@ const contact = {
         email : document.querySelector(".contact-email"),
         message : document.querySelector(".contact-text"),
         emailState : document.getElementById("input-hidden"),
-        server_url : "https://matthieu-degny-portfolio.site"
+        server_url : "http://localhost:3000"
 
     },
 
+
+    /*press button trigger the submission*/
     listener: function(){
-         contact.dataForm.contactForm.addEventListener('submit', contact.actionListener.handleSubmit)
+        contact.dataForm.contactForm.addEventListener('submit', contact.actionListener.handleSubmit)
     },
     
-
+    /*the submission handlesubmit the url (/) with ther server.js file
+    no data used here i send back only one alert and clean the form board*/
     actionListener: {
-
         handleSubmit : (e) => {
             e.preventDefault();
-
-                fetch(contact.dataForm.server_url,{
-                    method:"POST",
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(
-                        {
-                            datas: {
-                                email: contact.dataForm.email.value,
-                                message: contact.dataForm.message.value,           
-                            }
-                        }
-                        )
+            //check the statut of the request sendMail
+            //and send the formdatas through the body (protection of the datas)
+            fetch(contact.dataForm.server_url,{
+                //specify the method
+                method:"POST",
+                //specify content-type
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                //formDatas are send and change in json from the body
+                body: JSON.stringify({
+                    datas: {
+                        email: contact.dataForm.email.value,
+                        message: contact.dataForm.message.value                  
                     }
-                )
-
+                })
+            })
             .then(res => {
-
+                //if i got a positiv response from nodemailer
                 if(res.ok){
                     res.json()
-                    .then(data => {            
+                    .then(data => {
+                        console.log(data)
+                        console.log(res)                
                         window.alert("Votre email a bien été envoyé")
                         contact.dataForm.email.value = '';
                         contact.dataForm.message.value = '';
                     })
                 }
+                // if i got a negative response from nodemailer
                 else{
+                    console.log(res)
                     console.log("There is a problem with nodemailer")
                     contact.errorResponse()
                 }
@@ -66,4 +72,5 @@ const contact = {
         contact.dataForm.message.value = '';
     }
 }
+// when the loading is finish i launch contact.init
 document.addEventListener('DOMContentLoaded', contact.initContact);
