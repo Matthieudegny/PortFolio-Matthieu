48ab486b5420b909cc2483433ac5912240639107
Matt <matthieu.dev.degny@gmail.com>
2022-06-12 12:41:03 +0200

    console.log removed

--- public/js/contact.js
+++ public/js/contact.js
@@ -12,26 +12,20 @@ const contact = {
     },
 
 
-    /*press button trigger the submission*/
     listener: function(){
         contact.dataForm.contactForm.addEventListener('submit', contact.actionListener.handleSubmit)
     },
     
-    /*the submission handlesubmit the url (/) with ther server.js file
-    no data used here i send back only one alert and clean the form board*/
     actionListener: {
         handleSubmit : (e) => {
             e.preventDefault();
-            //check the statut of the request sendMail
-            //and send the formdatas through the body (protection of the datas)
+
             fetch(contact.dataForm.server_url,{
-                //specify the method
                 method:"POST",
-                //specify content-type
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                //formDatas are send and change in json from the body
+
                 body: JSON.stringify({
                     datas: {
                         email: contact.dataForm.email.value,
@@ -40,20 +34,16 @@ const contact = {
                 })
             })
             .then(res => {
-                //if i got a positiv response from nodemailer
                 if(res.ok){
                     res.json()
-                    .then(data => {
-                        console.log(data)
-                        console.log(res)                
+                    .then(data => {            
                         window.alert("Votre email a bien été envoyé")
                         contact.dataForm.email.value = '';
                         contact.dataForm.message.value = '';
                     })
                 }
-                // if i got a negative response from nodemailer
                 else{
-                    console.log(res)
+
                     console.log("There is a problem with nodemailer")
                     contact.errorResponse()
                 }
